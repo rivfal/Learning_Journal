3cd94de1c31189ff1cb5d345ce4284ace75c294e
Muhamad Rifai Arif <m.rifai.arif@gmail.com>
2022-05-19 19:22:43 +0800

    Day3

---
+++ phase_0/week_1/Day_3/moduleTes2.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    a = 'hallo, nama saya' + name
+    return a
+
+person1 = {
+    "name" : "John",
+    "age" : 36,
+    "country" : "Norway"}
---
+++ phase_0/week_1/Day_3/tes2.py
@@ -0,0 +1,4 @@
+import moduleTes2 as mt
+
+a = mt.person1['age']
+print(a)
